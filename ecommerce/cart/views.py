f4905839f8660a802c803fe9c835d5a044879a23
BHARATH-16-04-2001 <bharath.g.s115@gmail.com>
2025-02-21 22:37:39 +0530

    added update quantity in the cart

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -53,6 +53,23 @@ def cart_add(request):
 def cart_delete(request):
   pass
 
+
+
+
+
+
 def cart_update(request):
-  pass
+  cart = Cart(request)
+  
+  #test for post
+  if request.POST.get('action') == 'post':
+    product_id = int(request.POST.get('product_id'))
+    product_qty =  int(request.POST.get('product_qty'))
+
+    # calling update function
+    cart.update(product=product_id, quantity=product_qty)
+
+    response =  JsonResponse({'qty':product_qty})
+    return response
+
  
