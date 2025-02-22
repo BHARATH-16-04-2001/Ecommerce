66219a9de8342db7a4ac5e177a51a8abb21998a2
BHARATH-16-04-2001 <bharath.g.s115@gmail.com>
2025-02-22 10:46:47 +0530

    added cart_summary  functionality

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -20,6 +20,10 @@ def product(request, pk):
   product = Product.objects.get(id=pk)
   return render(request, 'product.html', {'product':product})
 
+def category_summary(request):
+  categories = Category.objects.all()
+  return render(request, 'category_summary.html', {'categories':categories})
+
 
 def category(request, foo):
   # to raplace "-" with " " spaces in urls
