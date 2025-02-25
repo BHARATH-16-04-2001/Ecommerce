b6317f3bd6530a668f02996dbb66034b660f6258
BHARATH-16-04-2001 <bharath.g.s115@gmail.com>
2025-02-25 18:53:45 +0530

    added the billing form and shipping form

--- ecommerce/payment/forms.py
+++ ecommerce/payment/forms.py
@@ -20,3 +20,14 @@ class ShippingFrom(forms.ModelForm):
 		exclude = ['user', ]
 
 
+class PaymentForm(forms.Form):
+	card_name =	 forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name on card'}), required=True)
+	card_number =  forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Card Number'}), required=True)
+	card_cvv_number =  forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'CVV Number'}), required=True)
+	card_expDate =  forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Card Expire Date'}), required=True)
+	card_address1 =  forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing address1'}), required=True)
+	card_address2 =  forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing address2 '}), required=True)
+	card_city =  forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing City '}), required=True)
+	card_state =  forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing State '}), required=True)
+	card_zipcode =  forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing Zipcode '}), required=True)
+	card_country =  forms.CharField(label="",  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Billing Country '}), required=True)
--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save
 
 
 # Create your models here.
@@ -21,6 +22,17 @@ class ShippingAddress(models.Model):
 
   def __str__(self):
     return f"Shipping Address - {str(self.id)} "
+  
+
+  
+# create the user shipping addesss when user signs up
+def create_shipping(sender, instance, created, **kwargs):
+  if created:
+    user_shipping = ShippingAddress(user = instance)
+    user_shipping.save()
+
+# to automate the profile stuff
+post_save.connect(create_shipping, sender = User)    
   
 
 
