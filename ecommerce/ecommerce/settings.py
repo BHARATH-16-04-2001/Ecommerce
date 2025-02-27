96a175f8aa613d013d0c41c72cfdbf4e8d6e81ae
BHARATH-16-04-2001 <bharath.g.s115@gmail.com>
2025-02-27 19:33:25 +0530

    Upgraded database from sqlite3 to postgresql

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -12,9 +12,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+#Load our environment varibales
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -81,8 +86,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': os.environ.get('DB_PASSWORD') ,
+        'HOST': 'bharath-bharath-4b95.f.aivencloud.com' ,
+        'PORT': '23318' ,
     }
 }
 
