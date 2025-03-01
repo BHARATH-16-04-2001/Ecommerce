e4453f5d9282de53eb4b0daa19086f64e8df06ed
Bharath_G_S <bharath.g.s115@gmail.com>
2025-03-01 12:04:38 +0530

    settings.py updated

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -94,15 +94,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'defaultdb',
-        # 'USER': 'avnadmin',
-        # 'PASSWORD': os.environ.get('DB_PASSWORD'),
-        # 'HOST': 'bharath-bharath-4b95.f.aivencloud.com' ,
-        # 'PORT': '23318' ,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'bharath-bharath-4b95.f.aivencloud.com' ,
+        'PORT': '23318' ,
         
     }
 }
